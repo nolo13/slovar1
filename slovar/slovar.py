a6af10017f6b9a39a8529596efbfc25c1fb2dcc8
Opilane TTHK <opilane@tthk.ee>
2021-01-25 11:20:20 +0200

    Add project files.

---
+++ slovar/slovar.py
@@ -0,0 +1,39 @@
+def ae(f):
+    fail=open(f,"r",encording"ut-8-si")
+    for line in file:
+        lup.apppend(line.strige)
+        file.close 
+        return aeg
+
+    rus_list=loe_faillist("rus.txt")
+est_list=loe_faillist("est.txt")
+print(rus_list)
+print(est_list)
+
+
+rus_list=zapul("rus.txt")
+
+f=open ("rust.txt","r",encoding="utf-8-sig")
+file=open ("rust.txt","r",encoding="utf-8-sig")
+    lup=[]
+    for line in file:
+        aeg.append(line.strip)
+    file.close
+    return aeg 
+def salvesta_failisse(f):
+    fail=open(f,"a",encoding="ut-8-si")
+    fail.write(text+"\n")
+    fail.colose()
+    mas=[]
+while True:
+    v=input("перевод-1,новые слова-2,исправить ошибку - 3,проверка знаний-4")
+    if v=="1:"
+        tolkimine()
+    elif v=="2":
+        rus_sona=input("Введи слово на русском ")
+        est_sona0input("Sisseta sõna")
+        
+
+
+
+
